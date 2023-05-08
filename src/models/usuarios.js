e322c9cde4ebbf3b867f4b44446446aea849c33f
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-05-07 22:34:16 -0500

    Rutas
    Se separan las rutas en un archivo controlador para manejar las peticiones con métodos.

--- src/models/usuarios.js
+++ src/models/usuarios.js
@@ -1,16 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 const usuariosSchema = new mongoose.Schema({
   nombres: { type: String, required: true },
   apellidos: { type: String, required: true },
   correo: { type: String, required: true },
   password: { type: String, required: true },
-},{
-    writeConcern: {
-       w: 'majority',
-       j: true,
-       wtimeout: 1000
-    }
 });
 
-module.exports = mongoose.model('usuarios', usuariosSchema);
+module.exports = mongoose.model("usuarios", usuariosSchema);
