323ea6690effc168155b16437cceb552f0dab6cb
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-05-22 14:37:27 -0500

    Encriptar
    Se agrega la encriptación de la clave al momento de crear un nuevo usuario

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -3,32 +3,43 @@ const User = require("../models/User");
 
 //Controllers
 userCtrl.createUser = async (req, res) => {
-  const userData = req.body;
-  const newUser = new User(userData);
-  const savedUser = await newUser.save();
-  res.json({
-    status: "User saved",
-  });
+    const userData = req.body;
+    const newUser = new User({
+        name: userData.name,
+        lastName: userData.lastName,
+        email: userData.email,
+        password: userData.password,
+        pet: {
+            name: userData.pet.name,
+            feedingSchedule: userData.pet.feedingSchedule
+        }
+    });
+    newUser.password=await newUser.encryptPassword(newUser.password);
+
+    const savedUser = await newUser.save();
+    res.json({
+        status: "User saved",
+    });
 };
 
 userCtrl.getUsers = async (req, res) => {
-  const users = await User.find();
-  res.json(users);
+    const users = await User.find();
+    res.json(users);
 };
 
 userCtrl.getUser = async (req, res) => {
-  const { id } = req.params;
-  const UserById = await User.findById(id);
-  res.json(UserById);
+    const {id} = req.params;
+    const UserById = await User.findById(id);
+    res.json(UserById);
 };
 
 userCtrl.receiveDetectionData = (req, res) => {
-  const detectionData = req.body;
-  console.log("Datos de detección recibidos:", detectionData[0].label);
+    const detectionData = req.body;
+    console.log("Datos de detección recibidos:", detectionData[0].label);
 
-  res
-    .status(200)
-    .json({ message: "Datos de detección recibidos correctamente" });
+    res
+        .status(200)
+        .json({message: "Datos de detección recibidos correctamente"});
 };
 
 module.exports = userCtrl;
--- models/User.js
+++ models/User.js
@@ -1,52 +1,68 @@
 const mongoose = require("mongoose");
-const { Schema } = mongoose;
+const bcrypt = require('bcrypt');
+const {Schema} = mongoose;
 
-const feedingScheduleSchema = new mongoose.Schema({
-  monday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  tuesday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  wednesday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  thursday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  friday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  saturday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
-  sunday: [{ 
-    startTime: { type: Date, required: true },
-    endTime: { type: Date, required: true }
-  }],
+const feedingScheduleSchema = new Schema({
+    monday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    tuesday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    wednesday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    thursday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    friday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    saturday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
+    sunday: [
+        {
+            startTime: {type: Date, required: true},
+            endTime: {type: Date, required: true},
+        },
+    ],
 });
 
-const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  lastName: { type: String, required: true },
-  email: { type: String, required: true },
-  password: { type: String, required: true },
-  pet: {
-    name: { type: String, required: true },
-    feedingSchedule: { type: feedingScheduleSchema, required: true },
-  },
+const userSchema = new Schema({
+    name: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+    pet: {
+        name: {type: String, required: true},
+        feedingSchedule: {type: feedingScheduleSchema, required: true},
+    },
 });
 
+userSchema.methods.encryptPassword = async function (password) {
+    const salt = await bcrypt.genSalt(10);
+    return await bcrypt.hash(password, salt);
+};
 
-userSchema.methods.encryptClave = async(clave) => {
-      const salt = await bcrypt.genSalt(10);
-      return bcrypt.hash(clave, salt);
-  }
-  
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
