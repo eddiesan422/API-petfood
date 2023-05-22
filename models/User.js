1efd08a76867bc91a75b9c11240a3161ce856467
eddiesan118 <eddies.hernandezm@konradlorenz.edu.co>
2023-05-22 17:49:53 -0500

    cambio en el schema
    Se realizo conversión de datos en el schema

--- models/User.js
+++ models/User.js
@@ -1,60 +1,35 @@
 const mongoose = require("mongoose");
 const bcrypt = require('bcrypt');
-const {Schema} = mongoose;
+const { Schema } = mongoose;
+
+const feedingScheduleItemSchema = new Schema({
+    startTime: { type: String, required: true, set: convertToDateTime },
+    endTime: { type: String, required: true, set: convertToDateTime },
+});
+
+function convertToDateTime(value) {
+    const date = new Date(value);
+    return date.toString() !== 'Invalid Date' ? date : value;
+}
 
 const feedingScheduleSchema = new Schema({
-    monday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    tuesday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    wednesday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    thursday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    friday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    saturday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
-    sunday: [
-        {
-            startTime: {type: Date, required: true},
-            endTime: {type: Date, required: true},
-        },
-    ],
+    monday: [feedingScheduleItemSchema],
+    tuesday: [feedingScheduleItemSchema],
+    wednesday: [feedingScheduleItemSchema],
+    thursday: [feedingScheduleItemSchema],
+    friday: [feedingScheduleItemSchema],
+    saturday: [feedingScheduleItemSchema],
+    sunday: [feedingScheduleItemSchema],
 });
 
 const userSchema = new Schema({
-    name: {type: String, required: true},
-    lastName: {type: String, required: true},
-    email: {type: String, required: true},
-    password: {type: String, required: true},
+    name: { type: String, required: true },
+    lastName: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
     pet: {
-        name: {type: String, required: true},
-        feedingSchedule: {type: feedingScheduleSchema, required: true},
+        name: { type: String, required: true },
+        feedingSchedule: { type: feedingScheduleSchema, required: true },
     },
 });
 
