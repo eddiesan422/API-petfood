3d4fc1f8a9247ca8d931f7b3a31497baead66304
VicVapor <victorj.gutierrezp@konradlorenz.edu.co>
2023-05-26 16:16:34 -0500

    Schedule Cron
    Se crea la función para recibir en cron los horarios de la persona logeada en el front

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,4 @@
+const CronJob = require('cron').CronJob;
 const userCtrl = {};
 const User = require("../models/User");
 const jwt = require("jsonwebtoken");
@@ -46,13 +47,47 @@ userCtrl.getUser = async (req, res) => {
 userCtrl.receiveDetectionData = async (req, res) => {
     const detectionData = req.body;
     console.log("Datos de detección recibidos:", detectionData[0].label);
-    const userId=req.userId;
-    const user=await User.findById(userId);
-    console.log(user.pet.feedingSchedule.friday[0]);
-    res
-        .status(200)
-        .json({message: "Datos de detección recibidos correctamente backend"});
+    const userId = req.userId;
+    const user = await User.findById(userId);
+
+    // Obtén los horarios desde el objeto del usuario (ejemplo para el horario del viernes)
+    let startTime = user.pet.feedingSchedule.friday[0].startTime;
+    let endTime = user.pet.feedingSchedule.friday[0].endTime;
+
+    console.log(startTime, endTime); // Verificación de los horarios
+
+    // Iniciar el cronjob solo si los horarios están definidos
+    if (startTime && endTime) {
+        startCronJob(startTime, endTime);
+    }
+
+    res.status(200).json({ message: "Datos de detección recibidos correctamente en el backend" });
 };
+
+function startCronJob(startTime, endTime){
+    const job = new CronJob(
+        '*/2 * * * * *', // Ejecutar cada 2 segundos
+        function() {
+            const currentTime = new Date();
+            const start = new Date();
+            const end = new Date();
+            const [startHours, startMinutes] = startTime.split(':');
+            const [endHours, endMinutes] = endTime.split(':');
+
+            start.setHours(startHours, startMinutes, 0);
+            end.setHours(endHours, endMinutes, 0);
+
+            if (currentTime >= start && currentTime <= end) {
+                console.log('Este mensaje se mostrará cada 2 segundos dentro del horario especificado');
+            }
+        },
+        null,
+        true,
+        'America/Los_Angeles'
+    );
+
+    job.start();
+}
 
 userCtrl.getPrivateSchedule = async (req, res) => {
     try {
